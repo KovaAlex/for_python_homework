be4ea104457a1abaa0f3b046f9dbc1f136928ec5
KovaAlex <123964805+KovaAlex@users.noreply.github.com>
2023-05-25 11:44:15 +0300

    Update HW_7/task_36.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- HW_7/task_36.py
+++ HW_7/task_36.py
@@ -20,7 +20,7 @@
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for i in range(1, num_rows + 1):
         for j in range(1, num_columns + 1):
-            print(*list(map(operation, [i], [j])), end='\t')
+            print(operation(i, j), end='\t')
         print()
 
 
